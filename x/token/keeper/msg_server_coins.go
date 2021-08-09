0b29f7bb82f6086ad3bd0756feb962179dc0a7c7
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-08-09 18:01:14 +0900

    Store tokens in micro KRW format
    Use the Oracle to get the exchange rate and store
    the "given" uluna in microKRW format.
    
    If the Oracle can't find the exchange rate,
    just use a hardcoded one for demonstartion purposes.
    
    Note that the tokens are minted out of thin air. :)

--- x/token/keeper/msg_server_coins.go
+++ x/token/keeper/msg_server_coins.go
@@ -8,7 +8,32 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/terra-money/core/x/token/types"
+	core "github.com/terra-money/core/types"
 )
+
+func GetMicroKRW (amountStr string, k msgServer, ctx sdk.Context) (sdk.Dec, error) {
+	amount, err := strconv.ParseUint(amountStr, 10, 64);
+	if err != nil {
+		return sdk.Dec{}, sdkerrors.Wrap(types.ErrInvalidAmount, "Amount must be a non-negative number")
+	}
+
+	lunaAmount := sdk.NewInt64DecCoin(core.MicroLunaDenom, int64(amount))
+	exRate, err := k.OracleKeeper.GetLunaExchangeRate(ctx, core.MicroKRWDenom)
+	if err != nil {
+		// use an assumed amount if the Oracle isn't running (just for demonstration purposes)
+		exRate = sdk.NewDec(16944000)
+
+		// .. but normally we'd return an error
+		// return nil, sdkerrors.Wrap(market.ErrNoEffectivePrice, "No exchange rate for KRW <-> Luna")
+	}
+
+	ukrwAmount := lunaAmount.Amount.Mul(exRate)
+	if ukrwAmount.LTE(sdk.ZeroDec()) {
+		return sdk.Dec{}, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "Value is less than zero")
+	}
+
+	return ukrwAmount, nil
+}
 
 func (k msgServer) CreateCoins(goCtx context.Context, msg *types.MsgCreateCoins) (*types.MsgCreateCoinsResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
@@ -22,10 +47,15 @@ func (k msgServer) CreateCoins(goCtx context.Context, msg *types.MsgCreateCoins)
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "index already set")
 	}
 
+	amount, error := GetMicroKRW(msg.Amount, k, ctx)
+	if error != nil {
+		return nil, error
+	}
+
 	var coins = types.Coins{
 		Creator: msg.Creator,
 		User:    msg.User,
-		Amount:  msg.Amount,
+		Amount:  amount.String(),
 	}
 
 	k.SetCoins(
@@ -52,22 +82,29 @@ func (k msgServer) UpdateCoins(goCtx context.Context, msg *types.MsgUpdateCoins)
 		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
 	}
 
+	newAmount, error := GetMicroKRW(msg.Amount, k, ctx)
+
+	if error != nil {
+		return nil, error
+	}
+
 	// panics on invalid input as validation should have already been done
-	getValidAmount := func (input string) uint64 {
-		if amount, err := strconv.ParseUint(msg.Amount, 10, 64); err == nil {
+	getValidAmount := func (input string) sdk.Dec {
+		if amount, err := sdk.NewDecFromStr(input); err == nil {
 			return amount
 		} else {
-			panic(fmt.Sprintf("Value not an integer: %v", msg.Amount))
+			panic(fmt.Sprintf("Value is not stored as a decimal: %v", msg.Amount))
 		}
 	}
 
 	// add previous coins
-	totalAmount := getValidAmount(msg.Amount) + getValidAmount(valFound.Amount)
+	prevAmount := getValidAmount(valFound.Amount)
+	totalAmount := newAmount.Add(prevAmount)
 
 	var coins = types.Coins{
 		Creator: msg.Creator,
 		User:    msg.User,
-		Amount:  strconv.FormatUint(totalAmount, 10),
+		Amount:  totalAmount.String(),
 	}
 
 	k.SetCoins(ctx, coins)
